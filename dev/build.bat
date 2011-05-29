21141804ae59f453b9eea8977c557a22ca8ef2e6
Vibhaj Rajan <vibhaj8@gmail.com>
2011-05-30 01:15:55 +0530

    v0.0.2

--- dev/build.bat
+++ dev/build.bat
@@ -1,3 +1,3 @@
-type init.js views\*.js renderers\*.js modules\*.js > ..\js\jquery-restclient.js
-type templates\*.js > ..\tpl\templates-restclient.js
+type init.js views\*.js renderers\*.js modules\*.js > ..\ui\js\jquery-restclient.js
+type templates\*.js > ..\ui\tpl\templates-restclient.js
 pause
