9e76030af39fe081537eb52eddb1ed3a557f7101
Vibhaj Rajan <vibhaj8@gmail.com>
2011-05-31 20:23:58 +0530

    v0.0.3

--- ui/js/jquery-restclient.js
+++
@@ -1,5 +0,0 @@
-RESTClient = {};
-RESTClient.jquery = {};
-RESTClient.jquery.view = {};
-RESTClient.jquery.module = {};
-RESTClient.jquery.renderer = {};
--- ui/tpl/templates-restclient.js
+++
@@ -1,6 +0,0 @@
-TestTemplate = (function(){
-	var tpl = $.template( '<p id="abc">Name: ${name}</p>'
-						+'<p>Time: ${time}</p>' );
-	tpl.compile();
-	return tpl;
-})();
