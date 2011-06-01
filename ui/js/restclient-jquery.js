5ace2d4a4a578a78e917e397c39aafa4ca0f2f74
Vibhaj Rajan <vibhaj8@gmail.com>
2011-06-01 23:46:30 +0530

    v0.0.5

--- ui/js/restclient-jquery.js
+++ ui/js/restclient-jquery.js
@@ -3,4 +3,4 @@ RESTClient.jquery = {};
 RESTClient.jquery.view = {};
 RESTClient.jquery.module = {};
 RESTClient.jquery.renderer = {};
-/** *	LinkButton module * *	@param selector string ***/RESTClient.jquery.module.LinkButton = (function(){	return {		execute : function(args){			$(args.selector).button();			$(args.selector).click(function(){				ServiceClient.client.Kernel.paint({					task : {						view : 'tabui',						template : 'test',						renderer : 'tplui'					},					params : {						tabtitle : 'Testing',						isclosable : true,						autoload : false,						loadurl : 'core/test.json',						loadparams : {}					}				});				return false;			});		}	};})();
+/** *	LinkButton module * *	@param selector string ***/RESTClient.jquery.module.LinkButton = (function(){	return {		execute : function(args){			$(args.selector).button();		}	};})();
