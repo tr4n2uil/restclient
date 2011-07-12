fcf83ee1a568959643b327d16791f80cefd4bf4a
Vibhaj Rajan <vibhaj8@gmail.com>
2011-07-12 21:46:56 +0530

    v0.1.8 : edited session.update usage

--- ui/js/restclient-templates.js
+++ ui/js/restclient-templates.js
@@ -7,7 +7,7 @@ RESTClient.jquery.template.Account = $.template('\
 	<p class="headline">Account</p>\
 	<ul>\
 		{{if RESTClient.session.user}}\
-		<li><a class="navigate" href="#session:cntr=#account-panel:sel=#login-container">Logout</a></li>\
+		<li><a class="navigate" href="#session">Logout</a></li>\
 		{{else}}\
 		<li><a class="navigate" href="#htmlload:cntr=#account-panel:url=ui/html/login.html">Login</a></li>\
 		{{/if}}\
