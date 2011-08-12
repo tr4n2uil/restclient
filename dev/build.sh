1fc0b4693dab0576be1bf822c5bba7598dc25891
Vibhaj Rajan <vibhaj8@gmail.com>
2011-08-12 22:51:57 +0530

    v0.2.8 : fixed issues in edit/delete resource

---
+++ dev/build.sh
@@ -0,0 +1,4 @@
+cat init.js services/*.js workflows/*.js helpers/*.js > ../ui/js/restclient-jquery.js
+cat templates/*.js > ../ui/js/restclient-templates.js
+cat styles/*.css > ../ui/css/restclient-styles.css
+pause
