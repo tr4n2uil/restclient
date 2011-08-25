bc3d6fde4e0e56185461e3269dfeb52ff5ecb117
Vibhaj Rajan <vibhaj8@gmail.com>
2011-08-25 20:21:13 +0530

    v0.3.0 : upgraded to FireSpark v0.4.3 with History Navigation and Persistent URL Support

--- dev/build.bat
+++ dev/build.bat
@@ -3,5 +3,5 @@ type init.js services\*.js workflows\*.js helpers\*.js > ..\ui\js\restclient-jqu
 type templates\*.js > ..\ui\js\restclient-templates.js
 type styles\*.css > ..\ui\css\restclient-styles.css
 type ..\ui\js\jquery-1.6.1.min.js ..\ui\js\jquery-ui-1.8.13.min.js ..\ui\js\jquery.tmpl.min.js ..\ui\js\json2.js ..\ui\js\jquery-firespark.js ..\ui\js\restclient-jquery.js ..\ui\js\restclient-templates.js > ..\ui\js\restclient-ui.js
-type ..\ui\css\default.css ..\ui\css\jquery.css ..\ui\css\jquery.css ..\ui\css\restclient-styles.css > ..\ui\css\restclient-ui.css
+type ..\ui\css\default.css ..\ui\css\jquery.css ..\ui\css\restclient-styles.css > ..\ui\css\restclient-ui.css
 pause
